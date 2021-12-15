3e217027bb35d4b68ea17c3dde7a5bace1bbc932
unknown <promadg@gmail.com>
2021-12-15 22:16:04 +0530

    Header Complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react/cjs/react.production.min";
+import Header from "./Header";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header />
+      <div className="component-body-container">
+        <button className="custom-btn add-btn">Add</button><br />
+        <div className="grid-container heading-container">
+          <span className="grid-item name-heading">Name</span>
+          <span className="grid-item phone-heading">Phone</span>
+        </div>
+        <div className="grid-container">
+          <span className="grid-item">Shilpa</span>
+          <span className="grid-item">999999999</span>
+        </div>
+        <div className="grid-container">
+          <span className="grid-item">Sara</span>
+          <span className="grid-item">888888888</span>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./Header.css"
+
+
+// const Header = function () {
+//     <div className="header">
+//         Phone Directory
+//     </div>
+// }
+
+class Header extends Component{
+    render(){
+        return(
+            <div className="header">
+                Phone Directory
+            </div>
+        )
+    }
+}
+export default Header;
